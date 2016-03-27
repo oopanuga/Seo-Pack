25bc0cce430b44432a1d4edcf31564031eccd7c5
oopanuga <oopanuga@gmail.com>
2016-03-28 00:49:59 +0100

    Newly added files due to file move

---
+++ SeoPack/Html/OpenGraph/OgAudio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SeoPack.Html.OpenGraph
+{
+    public class OgAudio
+    {
+        public OgAudio(Uri url)
+        {
+            Url = url;
+        }
+
+        [OgProperty("audio")]
+        public Uri Url { get; private set; }
+
+        [OgProperty("audio:secure_url")]
+        public Uri SecureUrl { get; private set; }
+
+        [OgProperty("audio:type")]
+        public string ImageType { get; set; }
+
+        public override string ToString()
+        {
+            return Url.AbsoluteUri;
+        }
+    }
+}
---
+++ SeoPack/Html/OpenGraph/OgImage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SeoPack.Html.OpenGraph
+{
+    public class OgImage
+    {
+        public OgImage(Uri url)
+        {
+            Url = url;
+        }
+
+        [OgProperty("image")]
+        public Uri Url { get; private set; }
+
+        [OgProperty("image:secure_url")]
+        public Uri SecureUrl { get; private set; }
+
+        [OgProperty("image:type")]
+        public string ImageType { get; set; }
+
+        [OgProperty("image:width")]
+        public int Width { get; set; }
+
+        [OgProperty("image:height")]
+        public int Height { get; set; }
+
+        public override string ToString()
+        {
+            return Url.AbsoluteUri;
+        }
+    }
+}
