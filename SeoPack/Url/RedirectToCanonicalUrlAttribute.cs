b08a91c37bab5b08174768fdec0c789741fb10a6
oopanuga <oopanuga@gmail.com>
2016-05-08 21:42:47 +0100

    Handle actions with NoTrailingSlashAttribute

--- SeoPack/Url/RedirectToCanonicalUrlAttribute.cs
+++ SeoPack/Url/RedirectToCanonicalUrlAttribute.cs
@@ -18,13 +18,69 @@ namespace SeoPack.Url
                 throw new ArgumentNullException("filterContext");
             }
 
-            var url = filterContext.RequestContext.HttpContext.Request.Url.AbsoluteUri;
-            var canonicalUrl = new CanonicalUrl(url).Value.AbsoluteUri;
+            var requestUrl = filterContext.RequestContext
+                .HttpContext.Request.Url.AbsoluteUri;
 
-            if(!url.Equals(canonicalUrl))
+            string canonicalUrl;
+            if (Requires301Redirect(requestUrl, HasNoTrailingSlashAttribute(filterContext), out canonicalUrl))
             {
-                filterContext.Result = new RedirectResult(canonicalUrl, true);
+                Do301Redirect(filterContext, canonicalUrl);
             }
+        }
+
+        protected virtual bool Requires301Redirect(string requestUrl, 
+            bool hasNoTrailingSlashAttribute, out string canonicalUrl)
+        {
+            bool requires301Redirect = false;
+
+            var canonicalUri = new CanonicalUrl(requestUrl).Value;
+            canonicalUrl = canonicalUri.AbsoluteUri;
+            var urlPath = canonicalUri.LocalPath;
+
+            if (hasNoTrailingSlashAttribute)
+            {
+                if (urlPath.LastIndexOf('/') == urlPath.Length - 1)
+                {
+                    canonicalUrl = string.Format("{0}://{1}{2}{3}",
+                                                          canonicalUri.Scheme,
+                                                          canonicalUri.Authority,
+                                                          urlPath.TrimEnd('/'),
+                                                          canonicalUri.Query);
+                }
+
+                if (!requestUrl.Equals(canonicalUrl))
+                {
+                    requires301Redirect = true;
+                }
+            }
+            else if (!requestUrl.Equals(canonicalUrl))
+            {
+                requires301Redirect = true;
+            }
+
+            return requires301Redirect;
+        }
+
+        /// <summary>
+        /// Checks if the action or controller the action belongs to has the NoTrailingSlashAttribute
+        /// </summary>
+        /// <param name="filterContext">The filter context</param>
+        /// <returns></returns>
+        protected bool HasNoTrailingSlashAttribute(AuthorizationContext filterContext)
+        {
+            var actionDescriptor = filterContext.ActionDescriptor;
+            return actionDescriptor.IsDefined(typeof(NoTrailingSlashAttribute), false) || 
+                actionDescriptor.ControllerDescriptor.IsDefined(typeof(NoTrailingSlashAttribute), false);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filterContext"></param>
+        /// <param name="canonicalUrl"></param>
+        protected void Do301Redirect(AuthorizationContext filterContext, string canonicalUrl)
+        {
+            filterContext.Result = new RedirectResult(canonicalUrl, true);
         }
     }
 }
